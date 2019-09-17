7a3c1fe6494e775d73cca5256d6ca80a606440c6
Rosa-Petursdottir <54938845+Rosa-Petursdottir@users.noreply.github.com>
2019-09-17 14:40:48 +0000

    Update tile_traveller.py

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,3 +1,31 @@
 # Functions for each movement - North, South, East and West
 # Function that updates the position
 # Function that checks if the movement wanted is possible
+
+# Functions for each movement - North, South, East and West
+# Function that updates the position
+# Function that checks if the movement wanted is possible
+
+def north(first,second) :
+    if second < 3 :
+        second += 1 
+    
+    return first, second
+
+def south(first,second) :
+    if second > 1 :
+        second -= 1 
+
+    return first, second
+
+def west(first,second) :
+    if first > 1 :
+        first -= 1 
+    
+    return first, second
+
+def east(first,second) :
+    if first < 3 :
+        first += 1 
+
+    return first, second
